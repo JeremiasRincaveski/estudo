41b6fad7050a1aa50fdebb1a6b43bb1a1214a95e
JeremiasRincaveski <jrincaveski@gmail.com>
2022-10-17 18:15:06 -0300

    sem comentarios

---
+++ BeeCrowd/ExtremamenteBasico.js
@@ -0,0 +1,6 @@
+var a = window.prompt()
+var b = window.prompt()
+
+var x = parseInt(a) + parseInt(b)
+
+document.write('X = ' + x)
---
+++ Estacio/html/aula9/teste.js
@@ -0,0 +1,2 @@
+prompt("Please enter some text",
+                "GeeksforGeeks");
